c73ad36cab947762cd3716422d324def0ca3c89c
vikzh <vikt.anmail@gmail.com>
2021-08-08 15:38:36 +0300

    visuals added

---
+++ init.el
@@ -0,0 +1,6 @@
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(blink-cursor-mode -1)
+;; makes sure the contents of the buffer is refreshed automatically when the file is changed outside of Emacs
+(global-auto-revert-mode t)
