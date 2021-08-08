cd0b51f870e594fb32553bd30d1586fef4cce57a
vikzh <vikt.anmail@gmail.com>
2021-08-08 16:31:42 +0300

    basic

--- init.el
+++ init.el
@@ -1,3 +1,8 @@
+;; Check if system is Darwin/macOS
+(defun is-macos ()
+  "Return true if system is darwin-based (Mac OS X)"
+  (string-equal system-type "darwin"))
+
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
@@ -5,3 +10,26 @@
 ;; makes sure the contents of the buffer is refreshed automatically when the file is changed outside of Emacs
 (global-auto-revert-mode t)
 (load-theme 'tsdh-light)
+
+;; C - Control
+;; Super - Command
+;; Meta - Option
+(when (is-macos)
+  (setq
+   mac-right-command-modifier 'super
+   mac-command-modifier 'super
+   mac-option-modifier 'meta
+   mac-left-option-modifier 'meta
+   mac-right-option-modifier 'meta
+   ;;mac-right-option-modifier 'nil
+   ))
+
+
+(global-set-key (kbd "s-=") 'text-scale-increase)
+(global-set-key (kbd "s--") 'text-scale-decrease)
+
+
+(setq
+ inhibit-startup-message t         ; Don't show the startup message
+ inhibit-startup-screen t          ; or screen
+)
